3c84a79150f45c7ed405a4196b09af7c272d0ec7
TadeoDeveloper <adeon000099@gmail.com>
2026-01-02 19:17:34 -0600

    Listas y bucles for en python

---
+++ List.py
@@ -0,0 +1,12 @@
+playlist = [
+  'Porches - rangerover',
+  'Mount Eerie - You Swan, Go On',
+  'Carolyn Polachek - Look at Me Now',
+  'Pinegrove - Darkness',
+  'LVL UP - Spirit Was',
+  'Mitski - First Love / Late Spring'
+]
+
+## Es para recorrer la lista completa la diferencia con el for i in playlist es que en este caso i toma el valor del indice
+for i in range(len(playlist)):
+  print(playlist[i])
